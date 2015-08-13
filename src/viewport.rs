6ce6bead62cfe55c092fe595401d9cee931495a5
psyomn <lethaljellybean@gmail.com>
2015-08-13 02:24:50 -0400

    viewport: make more descriptive

--- src/viewport.rs
+++ src/viewport.rs
@@ -12,7 +12,15 @@ pub struct ViewportBuilder {
 
 impl ViewportBuilder {
     pub fn new() -> ViewportBuilder {
-        ViewportBuilder { width: SCREEN_WIDTH, height: SCREEN_HEIGHT }
+        ViewportBuilder { width: 0, height: 0 }
+    }
+
+    pub fn default_x(&self) -> ViewportBuilder {
+        ViewportBuilder { width: SCREEN_WIDTH, height: self.height }
+    }
+
+    pub fn default_y(&self) -> ViewportBuilder {
+        ViewportBuilder { width: self.width, height: SCREEN_HEIGHT }
     }
 
     pub fn finalize(&self) -> Viewport {
