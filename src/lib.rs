03177f0ae8d64bbbffd84ba4a1b157d19832be99
psyomn <lethaljellybean@gmail.com>
2015-08-13 02:25:08 -0400

    lib: update file lists

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,10 @@ pub mod battle;
 pub mod levelers;
 pub mod map;
 pub mod tile;
-pub mod session;
 pub mod storyline_constants;
 pub mod cli;
+pub mod rrpg_rustbox;
+pub mod viewport;
 
 #[test]
 fn test_map_tile_creation() {
